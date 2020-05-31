70c8d2ab545ceaabecb421f5dd6a93f1d2b1c651
Swagger <swagger@gmail.com>
2020-05-31 15:03:57 +0800

    add

---
+++ src/com/example/isSymmetric/Solution.java
@@ -0,0 +1,53 @@
+package com.example.isSymmetric;
+
+/**
+ * 101. 对称二叉树
+ *
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ *
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ *  
+ *
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ *
+ * 解决思路：镜像是要求 root.left.val == root.right.val && root.right.val == root.left.val;
+ * 如果不满足以上条件的话就不是镜像的，另外出去一些为空的情况，如果都为空，则返回 true，否则返回 false。
+ */
+class Solution {
+
+    // Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return isSymmetric(root, root);
+    }
+
+    private boolean isSymmetric(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return true;
+        if (t1 == null || t2 == null) return false;
+        return t1.val == t2.val
+                && isSymmetric(t1.left, t2.right)
+                && isSymmetric(t1.right, t2.left);
+    }
+}
